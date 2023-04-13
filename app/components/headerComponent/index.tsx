d603cbccee448b7695b513aa0d00631fa05013a8
99x <99x@99xs-MBP.lan>
2023-04-13 17:28:41 +0530

    added header component

---
+++ app/components/headerComponent/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Text, TouchableOpacity, View } from "react-native/types";
+
+import styles from './styles';
+
+export const HeaderComponent = (props: { title: string, rtlEnabled: boolean }) => {
+    const { title, rtlEnabled = false } = props;
+
+    return (
+        <View style={[styles.headerTitleContainer]}>
+            <View style={[styles.headerTitleContainer]}>
+                <Text style={[styles.headerTitle]}>{title}</Text>
+            </View>
+            {rtlEnabled && (<View style={styles.rtlBtnActionContainer}>
+                <TouchableOpacity style={styles.rtlButton} onPress={() => { }}>{"RTL"}</TouchableOpacity>
+            </View>)}
+        </View>
+    )
+};
---
+++ app/components/headerComponent/styles.tsx
@@ -0,0 +1,9 @@
+import { StyleSheet } from "react-native/types";
+
+export default StyleSheet.create({
+    headerContainer: {},
+    headerTitleContainer: {},
+    headerTitle: {},
+    rtlBtnActionContainer: {},
+    rtlButton: {}
+});
